7244c1f4a0466e5e9d6c3422b2873e157df1a4db
Tommy Cheung <tommy200401@gmail.com>
2021-10-20 11:08:51 +0800

    [Java Stream][Tommy]feat: updated streamfilter

---
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+        return numbers.stream().filter(number->number%2==0).collect(Collectors.toList());
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+        return words.stream().filter(word->word.endsWith("s")).collect(Collectors.toList());
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+
+        return numbers.stream().filter(anotherNumbers::contains).collect(Collectors.toList());
+    }
+
+}
