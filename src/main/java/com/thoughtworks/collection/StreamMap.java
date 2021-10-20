fe3bfc8f9ebb557be53d0dab8716d60b5e87cb59
Tommy Cheung <tommy200401@gmail.com>
2021-10-20 11:16:43 +0800

    [Java Stream][Tommy]feat: updated streammap

--- src/main/java/com/thoughtworks/collection/StreamMap.java
+++ src/main/java/com/thoughtworks/collection/StreamMap.java
@@ -13,14 +13,17 @@ public class StreamMap {
             "v", "w", "x", "y", "z");
 
     public List<Integer> getTriple(List<Integer> numbers) {
-        return null;
+
+        return numbers.stream().map(number->number*3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter(List<Integer> numbers) {
-        return null;
+
+        return numbers.stream().map(number->ALPHABET.get(number-1)).collect(Collectors.toList());
     }
 
     public List<Integer> mapLength(List<String> words) {
-        return null;
+
+        return words.stream().map(word->word.length()).collect(Collectors.toList());
     }
 }
