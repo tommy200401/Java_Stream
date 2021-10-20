a80b33cb92ac622b27e375e326d2c9ebb5a89a6f
Tommy Cheung <tommy200401@gmail.com>
2021-10-20 13:02:00 +0800

    [Java Stream][Tommy]feat: updated streamreduce

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,18 +1,21 @@
 package com.thoughtworks.collection;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number%2==1).reduce((a,b) -> b).orElse(null);
     }
 
     public String getLongest(List<String> words) {
-        return null;
+
+        return words.stream().reduce("", (word1, word2) -> word1.length() > word2.length() ? word1: word2);
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+
+        return words.stream().reduce("", (word1, word2) -> word1 + word2).length();
     }
 }
